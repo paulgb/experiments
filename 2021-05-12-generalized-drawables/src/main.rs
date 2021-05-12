292f392de7c2cd3fd9ef42c41dc1e935f4a6037d
Paul Butler <paulgb@gmail.com>
2021-05-12 13:48:41 -0400

    refactor circles to their own module in preparation to add others

--- 2021-05-12-generalized-drawables/src/main.rs
+++ 2021-05-12-generalized-drawables/src/main.rs
@@ -1,14 +1,15 @@
 use std::iter;
 
-use wgpu::util::DeviceExt;
-use wgpu::{
-    BlendComponent, BlendState, Buffer, Device, RenderPass, RenderPipeline, SwapChainDescriptor,
-};
+use wgpu::{Device, RenderPass, SwapChainDescriptor};
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
+
+use circle::{Circle, CirclesLayer};
+
+mod circle;
 
 trait Layer {
     type D: Drawable;
@@ -18,127 +19,6 @@ trait Layer {
 
 trait Drawable {
     fn draw<'a>(&'a self, render_pass: &mut RenderPass<'a>);
-}
-
-#[repr(C)]
-#[derive(Copy, Clone, Debug, bytemuck::Zeroable, bytemuck::Pod)]
-struct Circle {
-    position: [f32; 2],
-    color: [f32; 4],
-    radius: f32,
-}
-
-struct CirclesLayer {
-    data: Vec<Circle>,
-}
-
-impl CirclesLayer {
-    pub fn new(data: Vec<Circle>) -> Self {
-        CirclesLayer { data }
-    }
-}
-
-struct CirclesLayerDrawable {
-    render_pipeline: RenderPipeline,
-    instance_buffer: Buffer,
-    num_circles: u32,
-}
-
-impl Drawable for CirclesLayerDrawable {
-    fn draw<'a>(&'a self, render_pass: &mut RenderPass<'a>) {
-        render_pass.set_pipeline(&self.render_pipeline);
-        render_pass.set_vertex_buffer(0, self.instance_buffer.slice(..));
-        render_pass.draw(0..6, 0..self.num_circles);
-    }
-}
-
-impl Layer for CirclesLayer {
-    type D = CirclesLayerDrawable;
-
-    fn init_drawable(&self, device: &Device, sc_desc: &SwapChainDescriptor) -> Self::D {
-        let instance_buffer_desc = wgpu::VertexBufferLayout {
-            array_stride: std::mem::size_of::<Circle>() as wgpu::BufferAddress,
-            step_mode: wgpu::InputStepMode::Instance,
-            attributes: &[
-                wgpu::VertexAttribute {
-                    offset: 0,
-                    shader_location: 0,
-                    format: wgpu::VertexFormat::Float32x2,
-                },
-                wgpu::VertexAttribute {
-                    offset: std::mem::size_of::<[f32; 2]>() as wgpu::BufferAddress,
-                    shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x4,
-                },
-                wgpu::VertexAttribute {
-                    offset: std::mem::size_of::<[f32; 6]>() as wgpu::BufferAddress,
-                    shader_location: 2,
-                    format: wgpu::VertexFormat::Float32,
-                },
-            ],
-        };
-
-        let instance_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Instance buffer"),
-            contents: bytemuck::cast_slice(&self.data),
-            usage: wgpu::BufferUsage::VERTEX,
-        });
-
-        let render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
-                push_constant_ranges: &[],
-            });
-
-        let vs_module = device.create_shader_module(&wgpu::include_spirv!("shader.vert.spv"));
-        let fs_module = device.create_shader_module(&wgpu::include_spirv!("shader.frag.spv"));
-
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &vs_module,
-                entry_point: "main",
-                buffers: &[instance_buffer_desc],
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &fs_module,
-                entry_point: "main",
-                targets: &[wgpu::ColorTargetState {
-                    format: sc_desc.format,
-                    write_mask: wgpu::ColorWrite::ALL,
-                    blend: Some(BlendState {
-                        color: BlendComponent::OVER,
-                        alpha: BlendComponent::REPLACE,
-                    }),
-                }],
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                //cull_mode: Some(wgpu::Face::Back),
-                cull_mode: None,
-                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-                clamp_depth: false,
-                polygon_mode: wgpu::PolygonMode::Fill,
-                conservative: false,
-            },
-            depth_stencil: None,
-            multisample: wgpu::MultisampleState {
-                count: 1,
-                mask: !0,
-                alpha_to_coverage_enabled: false,
-            },
-        });
-
-        CirclesLayerDrawable {
-            render_pipeline,
-            instance_buffer,
-            num_circles: self.data.len() as u32,
-        }
-    }
 }
 
 struct State {
@@ -187,37 +67,36 @@ impl State {
         };
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
-        let mut drawables: Vec<Box<dyn Drawable>> = Vec::new();
-
-        drawables.push(Box::new(
-            CirclesLayer::new(vec![
-                Circle {
-                    position: [0.45, 0.45],
-                    radius: 0.1,
-                    color: [0.5, 1.0, 0.5, 1.],
-                },
-                Circle {
-                    position: [0., 0.],
-                    radius: 0.2,
-                    color: [0.6, 0.6, 0., 1.],
-                },
-                Circle {
-                    position: [0.4, 0.4],
-                    radius: 0.05,
-                    color: [0.7, 0., 0.4, 1.],
-                },
-            ])
-            .init_drawable(&device, &sc_desc),
-        ));
-
-        drawables.push(Box::new(
-            CirclesLayer::new(vec![Circle {
-                position: [-0.3, -0.4],
-                radius: 0.5,
-                color: [0.3, 0.6, 0.9, 1.],
-            }])
-            .init_drawable(&device, &sc_desc),
-        ));
+        let drawables: Vec<Box<dyn Drawable>> = vec![
+            Box::new(
+                CirclesLayer::new(vec![
+                    Circle {
+                        position: [0.45, 0.45],
+                        radius: 0.1,
+                        color: [0.5, 1.0, 0.5, 1.],
+                    },
+                    Circle {
+                        position: [0., 0.],
+                        radius: 0.2,
+                        color: [0.6, 0.6, 0., 1.],
+                    },
+                    Circle {
+                        position: [0.4, 0.4],
+                        radius: 0.05,
+                        color: [0.7, 0., 0.4, 1.],
+                    },
+                ])
+                .init_drawable(&device, &sc_desc),
+            ),
+            Box::new(
+                CirclesLayer::new(vec![Circle {
+                    position: [-0.3, -0.4],
+                    radius: 0.5,
+                    color: [0.3, 0.6, 0.9, 1.],
+                }])
+                .init_drawable(&device, &sc_desc),
+            ),
+        ];
 
         Self {
             surface,
@@ -302,14 +181,15 @@ fn main() {
                 if !state.input(event) {
                     match event {
                         WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                        WindowEvent::KeyboardInput { input, .. } => match input {
-                            KeyboardInput {
-                                state: ElementState::Pressed,
-                                virtual_keycode: Some(VirtualKeyCode::Escape),
-                                ..
-                            } => *control_flow = ControlFlow::Exit,
-                            _ => {}
-                        },
+                        WindowEvent::KeyboardInput {
+                            input:
+                                KeyboardInput {
+                                    state: ElementState::Pressed,
+                                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                                    ..
+                                },
+                            ..
+                        } => *control_flow = ControlFlow::Exit,
                         WindowEvent::Resized(physical_size) => {
                             state.resize(*physical_size);
                         }
